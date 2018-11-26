96495a2f18e0852b942d9383d017bd746d501c01
Wagner Chrystian <w.chrystian.rk@gmail.com>
2018-11-26 09:41:33 -0400

    API do Google Maps

--- Localizame/app/src/main/java/com/masami/localizame/GravarActivity.java
+++ Localizame/app/src/main/java/com/masami/localizame/GravarActivity.java
@@ -3,14 +3,22 @@ package com.masami.localizame;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 import android.view.View;
 
-public class GravarActivity extends Activity {
+public class GravarActivity extends Activity  {
+    private android.app.FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravar);
+        fragmentManager= getSupportFragmentManager();
+        android.app.FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.container, new MapsActivity(),"MapsFragment");
+        transaction.commitAllowingStateLoss();
     }
     public void voltarinicio (View view){
         Intent intent = new Intent(getApplicationContext(),PrincipalActivity.class);
